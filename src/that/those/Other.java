0b33abdf5d8c9d6236229d90169c3cebc67cef6a
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-08-18 16:52:06 +0200

    added getter and setter
    Signed-off-by: Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>

--- src/that/those/Other.java
+++ src/that/those/Other.java
@@ -3,6 +3,14 @@ package that.those;
 public class Other {
 	int val;
 
+	public int getVal() {
+		return val;
+	}
+
+	public void setVal(int val) {
+		this.val = val;
+	}
+
 	public Other(int va) {
 		super();
 		this.val = va;
