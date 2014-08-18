c28b121a3784579156ebadb09c3d6586560ace12
Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>
2014-08-18 17:16:09 +0200

    Added a subclass of Other
    Signed-off-by: Behrouz Babaki <Behrouz.Babaki@cs.kuleuven.be>

---
+++ src/that/those/YetAnother.java
@@ -0,0 +1,20 @@
+package that.those;
+
+public class YetAnother extends Other {
+
+	private int val2;
+	
+	public YetAnother(int val) {
+		super(val);
+	}
+
+	public int getVal2() {
+		return val2;
+	}
+
+	public void setVal2(int val2) {
+		this.val2 = val2;
+	}
+	
+	
+}
